f3b2c449d8ecb1a6c3e9e9cafbfa530a98b70e55
Noto <akbar.exp57@gmail.com>
2021-01-28 23:19:42 +0700

    bug dupplicate user to test assignment

--- server/repository/testTakerRepository.js
+++ server/repository/testTakerRepository.js
@@ -108,43 +108,38 @@ exports.createIfNotExist = (testtaker, data, type) =>{
                 let sql = "SELECT * FROM test_takers WHERE test_id = ? AND user_id = ?"
                 connection.query(sql, [data.test_id, data.user_id], (err, result)=>{
                     if(err)
-                    return resolve(false)
+                        return resolve(false)
                     
                     if(result && type === 0 && result[0].start === null && result[0].end === null){
-                        update      = null
+                        update      = true
                         test_takers = result[0] 
                     }else{
                         create = false
                     }
+
+                    if(update){
+                        let sql = "UPDATE test_takers SET start = ? WHERE id = ?"
+                        connection.query(sql, [testtaker.start, test_takers.id ], (err,result)=>{
+                            if(err)
+                                return resolve(false)
+                            
+                            return resolve(true)
+                        })
+                    }
+                    else if(create){
+                            let sql = "INSERT INTO test_takers(test_id, user_id, start, end, score) VALUES (?,?,?,?,?)"
+                            connection.query(sql, [testtaker.test_id , testtaker.user_id, testtaker.start, testtaker.end, testtaker.score], (err,result)=>{
+                                if(err)
+                                    return resolve(false)
+                                
+                                return resolve(true)
+                            })
+                    }
+                    connection.release()
                 })
-                connection.release()
+
             });
 
-
-            if(update){
-                db.getConnection((err,connection)=>{
-                    let sql = "UPDATE test_takers SET start = ? WHERE id = ?"
-                    connection.query(sql, [testtaker.start, test_takers.id ], (err,result)=>{
-                        if(err)
-                            return resolve(false)
-                        
-                        connection.release()
-                        return resolve(true)
-                    })
-                })
-            }
-            else if(create){
-                db.getConnection((err,connection)=>{
-                    let sql = "INSERT INTO test_takers(test_id, user_id, start, end, score) VALUES (?,?,?,?,?)"
-                    connection.query(sql, [testtaker.test_id , testtaker.user_id, testtaker.start, testtaker.end, testtaker.score], (err,result)=>{
-                        if(err)
-                            return resolve(false)
-                        
-                        connection.release()
-                        return resolve(true)
-                    })
-                })
-            }
         }catch(err){
             throw err
         }
